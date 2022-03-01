ffb609119a16e26fa10545acb4870c3ffd058a43
MeTi-DeV <mahdi_zarei78@yahoo.com>
2022-03-01 22:53:29 +0330

    Create Product Model and Fetch Data to App create  AppConstant class

--- lib/controllers/popular_product_controller.dart
+++ lib/controllers/popular_product_controller.dart
@@ -1,22 +1,19 @@
 import 'package:food_delivery_app/data/repository/popular_product_repo.dart';
+import 'package:food_delivery_app/model/product.dart';
 import 'package:get/get.dart';
-  //comment :  this class is for management our logics for list of popularProducts
+
 class PopularProductController extends GetxController {
-    //comment :  create instance of popularPoroductRepo
   final PopularPoroductRepo popularPoroductRepo;
 
   PopularProductController({required this.popularPoroductRepo});
-    //comment : create a list that will store all of popularProduct data received form data base
   List<dynamic> _popularProductList = [];
-    //comment : it's copy of _popularProductList for use it every where that we want in UI
   List<dynamic> get popularProductList => _popularProductList;
-  //comment : this function  will get all data that we want in popularPoroductRepo and if it was successful store them in 
-  // _popularProductList
   Future<void> getPopularProductList() async {
     Response response = await popularPoroductRepo.getPopularProductRepo();
     if (response.statusCode == 200) {
       _popularProductList = [];
-      // _popularProductList.addAll();
+      _popularProductList.addAll(Product.fromJson(response.body).products);
+      print(_popularProductList);
       update();
     } else {}
   }
--- lib/helper/dependencies.dart
+++ lib/helper/dependencies.dart
@@ -1,11 +1,11 @@
 import 'package:food_delivery_app/controllers/popular_product_controller.dart';
 import 'package:food_delivery_app/data/api/api_client.dart';
 import 'package:food_delivery_app/data/repository/popular_product_repo.dart';
+import 'package:food_delivery_app/utils/app_constants.dart';
 import 'package:get/get.dart';
-  //comment :  create this function to get all of data at first with run app and call it in main.dart
 Future<void> init() async {
   //api client
-  Get.lazyPut(() => ApiClient(appBaseUrl: "http//www.google.com"));
+  Get.lazyPut(() => ApiClient(appBaseUrl: AppConstants.BASE_URL));
   // repos
   Get.lazyPut(() => PopularPoroductRepo(apiClient: Get.find()));
   //controller
